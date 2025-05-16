b1413ec417297a7c1bf463b4e2ea69650e7eaa3c
CalvinLe08 <lenguyenkhang102002@gmail.com>
2025-05-16 19:28:31 +0700

    complete unit tests for account

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -2,29 +2,84 @@ package db
 
 import (
 	"context"
+	"database/sql"
 	"testing"
+	"time"
 
+	"github.com/calvinnle/simple-bank/util"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestCreateAccount(t *testing.T) {
+func createRandomAccount(t *testing.T) Account {
 	arg := CreateAccountParams{
-		Owner:    "calvin",
-		Balance:  100,
-		Currency: "USD",
+		Owner:    util.RandomOwner(),
+		Balance:  util.RandomMoney(),
+		Currency: util.RandomCurrency(),
 	}
 
 	account, err := testQueries.CreateAccount(context.Background(), arg)
-	
 
 	// use subpackage assert instead of require like in the course
-	assert.Nil(t, err);
+	assert.Nil(t, err)
 	assert.NotEmpty(t, account)
-	
+
 	assert.Equal(t, arg.Owner, account.Owner)
 	assert.Equal(t, arg.Balance, account.Balance)
 	assert.Equal(t, arg.Currency, account.Currency)
 
 	assert.NotZero(t, account.ID)
 	assert.NotZero(t, account.CreatedAt)
+
+	return account
+}
+
+func TestCreateAccount(t *testing.T) {
+	createRandomAccount(t)
+}
+
+func TestGetAccount(t *testing.T) {
+	account1 := createRandomAccount(t)
+	account2, err := testQueries.GetAccount(context.Background(), account1.ID)
+
+	assert.NoError(t, err)
+	assert.NotEmpty(t, account2)
+
+	assert.Equal(t, account1.ID, account2.ID)
+	assert.Equal(t, account1.Owner, account2.Owner)
+	assert.Equal(t, account1.Balance, account2.Balance)
+	assert.Equal(t, account1.Currency, account2.Currency)
+
+	assert.WithinDuration(t, account1.CreatedAt, account2.CreatedAt, time.Second)
+}
+
+func TestUpdateAccount(t *testing.T) {
+	account1 := createRandomAccount(t)
+
+	arg := UpdateAccountParams {
+		ID: account1.ID,
+		Balance: util.RandomMoney(),
+	}
+
+	account2, err := testQueries.UpdateAccount(context.Background(), arg)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, account2)
+
+	assert.Equal(t, account1.ID, account2.ID)
+	assert.Equal(t, account1.Currency, account2.Currency)
+	assert.Equal(t, arg.Balance, account2.Balance)
+	assert.Equal(t, account1.Owner, account2.Owner)
+	
+	assert.WithinDuration(t, account1.CreatedAt, account2.CreatedAt, time.Second)
+}
+
+func TestDeleteAccount(t *testing.T) {
+	account1 := createRandomAccount(t)
+
+	err := testQueries.DeleteAccount(context.Background(), account1.ID)
+	assert.NoError(t, err)
+
+	account2, err := testQueries.GetAccount(context.Background(), account1.ID)
+	assert.Error(t, err)
+	assert.EqualError(t, err, sql.ErrNoRows.Error())
+	assert.Empty(t, account2)
 }
